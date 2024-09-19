0c47375f03a4530952913843a6f6360aaf56619b
Dung Huynh Duc <dunghd.it@gmail.com>
2024-09-19 22:56:09 +0800

    feat: add new dependencies and update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [TanStackRouterVite({}), react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
